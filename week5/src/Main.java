99fb9dbc7bfbb911ee3b6daf886a26a9b4c9ad01
sefaend <huseyinsefaendes@posta.mu.edu.tr>
2025-03-21 12:07:49 +0300

    week-5

--- week5/src/Main.java
+++ week5/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Point p1 = new Point();
+        System.out.println(p1.xCoord);
+        System.out.println(p1.yCoord);
+        Point p2 = new Point(3,4);
+        System.out.println(p2.xCoord);
+        System.out.println(p2.yCoord);
+        Point p3 = new Point(5);
+        System.out.println(p3.xCoord);
+        System.out.println(p3.yCoord);
+
+        Rectangle r1 = new Rectangle(
+                p3,
+                4,
+                5
+        );
+        System.out.println(r1.getSideA());
+        System.out.println(r1.getSideB());
+        System.out.println(r1.getTopLeft().xCoord);
+        System.out.println(r1.getTopLeft().yCoord);
+
+        System.out.println(r1.perimeter());
+        System.out.println(r1.area());
+
     }
 }
